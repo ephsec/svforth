4df027677b52156f012a60e1880e357551c0cc6d
Wes Brown <wbrown@ephemeralsecurity.com>
2013-11-04 22:26:21 -0500

    Experiments to build the smallest possible Forth in JavaScript.

---
+++ experiments/minforth.js
@@ -0,0 +1,55 @@
+ctx = {
+	d: {
+		"dup": function() {
+			s.push( s.pop(), s.pop() );
+		},
+		"rot": function() {
+			s.push( s.shift() );
+		},
+		"-rot": function() {
+			s = [ s.pop() ].concat(s);
+		},
+		"swap": function() {
+			s.push( s.pop(), s.pop() );
+		},
+		"+": function() {
+			s.push( ( s.pop() + s.pop() ) );
+		},
+		"-": function() {
+			s.push( s.pop(), s.pop() );
+			s.push( s.pop() - s.pop() );
+		},
+		'*': function() {
+			s.push( s.pop() * s.pop() );
+		},
+		'/': function() {
+			s.push( s.pop(), s.pop() );
+			s.push( s.pop() / s.pop() );
+		},
+		'.s': function() {
+			console.log( s );
+		}
+	},
+	s: []
+};
+
+e = function(t) {
+	s = this.s;
+	d = this.d;
+
+	if ( t.constructor === String ) {
+		t = t.split(/\s/);
+	}
+	while ( t.length ) {
+		w = t.shift();
+		if ( w in d ) {
+			d[w].apply(this);
+		} else if ( !( isNaN( w ) ) ) {
+			s.push( parseFloat( w ) );
+		} else if ( w !== '' ) {
+			s.push( w );
+		};
+	}
+}
+
+e.apply(ctx, ['1 2 3 rot + 1 - 2 * 2 / 1 -rot .s']);
