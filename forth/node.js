eeda9fa36c31d47d2535c8300cc9446afe8804c1
Wes Brown <wbrown@ephemeralsecurity.com>
2013-11-06 15:50:26 -0500

    node.js now supports autoloading a site.f file, and incorporates server.js.

--- forth/node.js
+++ forth/node.js
@@ -4,15 +4,20 @@ process.stdin.setEncoding('utf8');
 var forth = require( '../forth.js' );
 var ds = require( './ds.js' );
 var url = require( './url.js' );
+var database = require( './database.js' );
+var server = require( './server.js' );
 
 forth.initialDictionary.registerWords( ds.DataStructureFns );
 forth.initialDictionary.registerWords( url.URLFns );
+forth.initialDictionary.registerWords( database.DatabaseFns );
+forth.initialDictionary.registerWords( server.ServerFns );
 
 console.log( forth.initialDictionary.definitions );
 
 initialContext = forth.createContext( { dictionary: forth.initialDictionary } );
 executionContext = forth.applyExecutionContext.apply( initialContext );
 executionContext.load( 'forth/rss.f' );
+executionContext.load( 'site.f' );
 
 function parseInput(data) {
     executionContext.execute(data);
---
+++ site.f
@@ -0,0 +1,3 @@
+( our site-specific autostart file for node.js svforth                         )
+
+start-server                     start our RPC and content server on port 8000 )
