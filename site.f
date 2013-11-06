36e288e753cae2481bcf3e5e7bd0ef90ce4dd5e4
Wes Brown <wbrown@ephemeralsecurity.com>
2013-11-06 17:49:21 -0500

    Add a default site.f for node.js that starts up the server.

--- site.f
+++ site.f
@@ -1,3 +1,3 @@
 ( our site-specific autostart file for node.js svforth                         )
 
-start-server                     start our RPC and content server on port 8000 )
+start-server                   ( start our RPC and content server on port 8000 )
